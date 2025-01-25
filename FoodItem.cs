fe98383e79a380606bf6bc1df33a3087d90ecf1d
CarsonTanner <you@example.com>
2025-01-24 17:53:59 -0700

    Add project files.

---
+++ FoodItem.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission_3
+{
+    internal class FoodItem
+    {
+        public string name;
+        public string category;
+        public int quantity;
+        public string expDate;
+
+        public FoodItem(string n, string c, int q, string e) {
+            name = n;
+            category = c;
+            quantity = q;
+            expDate = e;
+        }
+
+        public static List<FoodItem> AddItem(List<FoodItem> listItems)
+        {
+            string n;
+            string c;
+            int q;
+            string e;
+
+            Console.WriteLine("\nDonation Item Name: ");
+            n = Console.ReadLine();
+
+            Console.WriteLine("Donation Category: ");
+            c = Console.ReadLine();
+
+            Console.WriteLine("Quantity: ");
+            q = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Expiration Date: ");
+            e = Console.ReadLine();
+
+            FoodItem item = new FoodItem(n, c, q, e);
+
+            listItems.Add(item);
+
+            return listItems;
+        }
+
+        public static List<FoodItem> DeleteItem(List<FoodItem> listItems)
+        {
+            int id;
+
+            Console.WriteLine("\nEnter an ID of the item to delete: ");
+            id = int.Parse(Console.ReadLine());
+            listItems.RemoveAt(id);
+
+            return listItems;
+        }
+
+        public static void List(List<FoodItem> listItems)
+        {
+            for (int i = 0; i <= listItems.Count - 1; i++)
+            {
+                Console.WriteLine("\n" + i + " " + listItems[i].name + " " + listItems[i].category + " "
+                    + listItems[i].quantity + " " + listItems[i].expDate);
+            }
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using Mission_3;
+using System.Transactions;
+
+List<FoodItem> listItems = new List<FoodItem>();
+string name;
+string category;
+string expDate;
+string cmd = "";
+int quantity;
+int id;
+
+while (cmd != "exit")
+{
+
+    Console.WriteLine("\nAdd Food Items: add\n" +
+                      "Delete Food Items: delete\n" +
+                      "List Current Food Items: list\n" +
+                      "Exit Program: exit\n\n" +
+                      "Please type one of the commands above: ");
+
+    cmd = Console.ReadLine();
+
+    if (cmd.Equals("add")) 
+    {
+        listItems = FoodItem.AddItem(listItems);
+    }
+    else if (cmd.Equals("delete")) 
+    {
+        listItems = FoodItem.DeleteItem(listItems);
+    }
+    else if (cmd.Equals("list"))
+    {
+        FoodItem.List(listItems);
+    }
+    else if (cmd.Equals("exit")) 
+    {
+        return;
+    }
+    else
+    {
+        Console.WriteLine("\nInput Invalid");
+    }
+
+}
+
+
